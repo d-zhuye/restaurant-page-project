213b6e9020ee6766a4730fdfd1b24c2e8281bf2d
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-11-23 15:37:12 -0800

    Configure import paths from home-page

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// This  file serves as the entry point
+import "./styles.css";
+import {default as displayHomePage} from "../src/home-page/home-page";
+import "./home-page/home-page.css";
+
+displayHomePage();
